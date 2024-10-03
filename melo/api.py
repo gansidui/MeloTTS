c98fc0ff0eb3160c5c7e7ccb23ad133567fe38dd
gansidui <lijiegm@gmail.com>
2024-10-03 16:04:09 +0800

    分段的效果不行，而且有bug，先屏蔽掉，由上层处理分段

--- melo/api.py
+++ melo/api.py
@@ -82,7 +82,8 @@ class TTS(nn.Module):
 
     def tts_to_file(self, text, speaker_id, output_path=None, sdp_ratio=0.2, noise_scale=0.6, noise_scale_w=0.8, speed=1.0, pbar=None, format=None, position=None, quiet=False,):
         language = self.language
-        texts = self.split_sentences_into_pieces(text, language, quiet)
+        # texts = self.split_sentences_into_pieces(text, language, quiet)
+        texts = [text]
         audio_list = []
         if pbar:
             tx = pbar(texts)
