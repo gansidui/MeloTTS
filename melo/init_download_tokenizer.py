f84916a8138d8d7cc31b3f4fdd3872dc9672cc6e
gansidui <lijiegm@gmail.com>
2024-10-07 12:03:09 +0800

    增加 AutoModelForMaskedLM 下载

--- melo/init_download_tokenizer.py
+++ melo/init_download_tokenizer.py
@@ -1,5 +1,6 @@
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForMaskedLM
 
+# 下载 tokenizer
 
 # chinese
 model_id = 'bert-base-multilingual-uncased'
@@ -25,3 +26,26 @@ tokenizer = AutoTokenizer.from_pretrained(model_id)
 model_id = 'dccuchile/bert-base-spanish-wwm-uncased'
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
+
+
+# 下载 model
+
+# chinese
+model_id = 'hfl/chinese-roberta-wwm-ext-large'
+model = AutoModelForMaskedLM.from_pretrained(model_id)
+
+# english
+model_id = 'bert-base-uncased'
+model = AutoModelForMaskedLM.from_pretrained(model_id)
+
+# french
+model_id = 'dbmdz/bert-base-french-europeana-cased'
+model = AutoModelForMaskedLM.from_pretrained(model_id)
+
+# japanese
+model_id = 'tohoku-nlp/bert-base-japanese-v3'
+model = AutoModelForMaskedLM.from_pretrained(model_id)
+
+# spanish
+model_id = 'dccuchile/bert-base-spanish-wwm-uncased'
+model = AutoModelForMaskedLM.from_pretrained(model_id)
--- melo/test_infer.py
+++ melo/test_infer.py
@@ -1,11 +1,12 @@
 import os
 from api import TTS
+import time
 
-ckpt_path = 'models/mix_en/G_10000.pth'
+ckpt_path = 'models/mix_en/G_14.pth'
 text = """With fusion energy at its core, Virginia would become the epicenter of the next tech revolution. Companies would flood the state, drawn by the unlimited, cheap power. Research labs would push the boundaries of AI, robotics, and space exploration, all powered by this clean energy. Job creation would skyrocket, attracting talent from across the country. But could this explosion in growth come at a price? Stay tuned to see the unexpected consequences!"""
 language = 'EN'
 output_dir = 'output'
-speaker = 'en_001'
+speaker = 'en_148'
 
 def main(ckpt_path, text, language, output_dir):
     if ckpt_path is None:
@@ -19,7 +20,12 @@ def main(ckpt_path, text, language, output_dir):
         if spk_name == speaker:
             save_path = f'{output_dir}/{spk_name}/output.wav'
             os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+            start_time = time.time()
             model.tts_to_file(text, spk_id, save_path)
+            end_time = time.time()
+            used_time = (end_time - start_time) * 1000
+            print(f"tts_to_file used_time: {used_time:.2f} ms")
 
 if __name__ == "__main__":
     main(ckpt_path, text, language, output_dir)
